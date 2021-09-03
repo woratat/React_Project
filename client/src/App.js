096505511d65055c3acc3a2c0ec7c22caa1f48bc
Ratchapol <84052786+tumka@users.noreply.github.com>
2021-09-03 16:20:52 +0700

    create useBody and test

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,47 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { useBody } from './use';
+
+function Home() {
+  const [test, setTest] = useState('tat');
+  useBody(test);
+
+  return (
+    <div>
+      <h1>Tat</h1>
+      <button type="button" onClick={() => setTest('tat2')}>Click</button>
+      <Link to="/about">About</Link>
+    </div>
+  )
+}
+
+function About() {
+  useBody('tum');
+  return (
+    <div>
+      <h1>tum</h1>
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
+
+function Error() {
+  return (
+    <div>
+      <h1>Error</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route component={Error} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/use/index.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+const addBodyClass = (className) => {
+    document.body.classList.add(className);
+}
+
+const removeBodyClass = (className) => {
+    document.body.classList.remove(className);
+}
+
+const useBody = (className) => {
+    useEffect(() => {
+        className instanceof Array ? className.map(addBodyClass) : addBodyClass(className);
+
+        return () => {
+            className instanceof Array ? className.map(removeBodyClass) : removeBodyClass(className);
+        }
+    }, [className]);
+
+    
+}
+
+export { useBody };
