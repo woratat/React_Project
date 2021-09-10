2aeb3ae34ad8c54e84044d3c53d26a892911c305
Ratchapol <84052786+tumka@users.noreply.github.com>
2021-09-10 14:18:32 +0700

    setup App

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,11 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { useBody } from './use';
-
-function Home() {
-  const [test, setTest] = useState('tat');
-  useBody(test);
-
-  return (
-    <div>
-      <h1>Tat</h1>
-      <button type="button" onClick={() => setTest('tat2')}>Click</button>
-      <Link to="/about">About</Link>
-    </div>
-  )
-}
-
-function About() {
-  useBody('tum');
-  return (
-    <div>
-      <h1>tum</h1>
-      <Link to="/">Home</Link>
-    </div>
-  )
-}
-
-function Error() {
-  return (
-    <div>
-      <h1>Error</h1>
-    </div>
-  )
-}
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route component={Error} />
+        
       </Switch>
     </Router>
   );
