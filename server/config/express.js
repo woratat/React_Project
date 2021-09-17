945541a7d1aacb4611140b0b51181356f0d24ebf
woratat <84006033+woratat@users.noreply.github.com>
2021-09-17 16:45:37 +0700

    Create api post & update comment

--- server/config/express.js
+++ server/config/express.js
@@ -9,6 +9,8 @@ const getMovieDetail = require('../service/router/getMovieDetail');
 const getCarousel = require('../service/router/getCarousel');
 const getFavorite = require('../service/router/getMovieFavorite');
 const postFavorite = require('../service/router/postMovieFavorite');
+const postComment = require('../service/router/postComment');
+const updateComment = require('../service/router/updateComment');
 
 module.exports = () => {
     app.use(express.json());
@@ -37,6 +39,8 @@ module.exports = () => {
     app.use('/api/get/carousel', getCarousel);
     app.use('/api/get/favorite', getFavorite);
     app.use('/api/post/favorite', postFavorite);
+    app.use('/api/post/comment', postComment);
+    app.use('/api/update/comment', updateComment);
 
     return { listen };
 }
