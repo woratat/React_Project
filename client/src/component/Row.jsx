dd8c81986accbfc68000d5c1a7aef889e1ea9da3
Ratchapol <84052786+tumka@users.noreply.github.com>
2021-09-19 21:04:57 +0700

    update row

--- client/src/component/Row.jsx
+++ client/src/component/Row.jsx
@@ -1,7 +1,5 @@
-import React, { Fragment, useState } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import styles from "../assets/scss/row.module.scss";
-import { useImage } from "../use/";
 import CardMovie from "./CardMovie";
 function Row({ title, data }) {
   return (
@@ -10,7 +8,7 @@ function Row({ title, data }) {
 
       <div className={styles.row_posters}>
         {data.map((item) => {
-          return <CardMovie item={item} />;
+          return <CardMovie item={item} />
         })}
       </div>
     </div>
