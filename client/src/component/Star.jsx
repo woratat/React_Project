8b8e237a98f6dcf2cd027e0a936cc4b4eecf85e4
yacht2562 <Kasira_k@cmu.ac.th>
2021-09-20 14:21:44 +0700

    add star on allSeries & all movie
    add star below movie

--- client/src/component/Star.jsx
+++ client/src/component/Star.jsx
@@ -4,20 +4,13 @@ import styles from "../assets/scss/row.module.scss";
 
 export default function Star({ like }) {
   const [hover, setHover] = useState(null);
-  let rating = like / 2;
+  let rating = like;
   let ratingValue = 0;
   return (
     <div>
-      {[...Array(5)].map((i) => {
-        ratingValue = i + 1;
-        return (
-          <label>
-            <FaStar
-              className=""
-              color={ratingValue <= rating ? "#ffc107" : "#e4e5e9"}
-            />
-          </label>
-        );
+      {[...Array(10)].map(() => {
+        ratingValue += +1;
+        return <FaStar color={ratingValue <= rating ? "#ffc107" : "#e4e5e9"} />;
       })}
     </div>
   );
