236e3721a31d8f818822fcbcb32293248c723ec3
yacht2562 <Kasira_k@cmu.ac.th>
2021-09-20 17:13:52 +0700

    change li star
    change star to boxicon

--- client/src/component/Star.jsx
+++ client/src/component/Star.jsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
-import { FaStar } from "react-icons/fa";
-import styles from "../assets/scss/row.module.scss";
 
 export default function Star({ like }) {
   const [hover, setHover] = useState(null);
   let rating = like / 2;
   let ratingValue = 0;
+  const getColor = () => {
+    if (ratingValue <= rating) {
+      return "#ffc107";
+    } else {
+      return "#e4e5e9";
+    }
+  };
   return (
     <div>
       {[...Array(5)].map(() => {
         ratingValue += +1;
         return (
-          <FaStar
-            size={20}
-            color={ratingValue <= rating ? "#ffc107" : "#e4e5e9"}
-          />
+          <i className="bx bxs-star" style={{ color: getColor() }} size={20} />
         );
       })}
     </div>
